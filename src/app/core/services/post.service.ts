4d24b93c6459822fdedf16c8ebe839ea8f26dc75
Vadim <webdevelopers52@gmail.com>
2018-10-11 11:58:43 +0300

    feat add posts

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -8,13 +8,14 @@ import { map } from 'rxjs/operators';
 })
 export class PostService {
   private posts: {};
+  private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: HttpClient) { }
 
   getPosts(refresh?: boolean): Observable<any> {
     if(!refresh && this.posts) {
       return of(this.posts);
     }
-    return this.http.get<any>('https://jsonplaceholder.typicode.com/posts')
+    return this.http.get<any>(this.url)
       .pipe(map(posts => {
         this.posts = posts;
         return posts;
@@ -22,9 +23,21 @@ export class PostService {
   }
 
   getPost(postId: String): Observable<any> {
-    return this.http.get<any>(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    return this.http.get<any>(`${this.url}/${postId}`)
       .pipe(map(post => {
         return post;
       }));
+  }
+
+  createPost(post: Object): Observable<any> {
+    return this.http.post(this.url, post);
+  }
+
+  updatePost(id: String, post: Object): Observable<any> {
+    return this.http.put(this.url, post);
+  }
+
+  deletePost(id: String): Observable<any> {
+    return this.http.delete(this.url);
   }
 }
